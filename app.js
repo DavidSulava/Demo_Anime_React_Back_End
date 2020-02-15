4f4f355259b793bd2d68281a76e31988d150123d
David Sulava <sulavaperm@gmail.com>
2020-02-15 13:52:30 +0500

    changed allowed origin behavior 3

--- app.js
+++ app.js
@@ -52,6 +52,7 @@ app.use( function(req, res, next)
         res.header("Access-Control-Allow-Origin",  req.headers.origin ); // update to match the domain you will make the request from
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Range");
       }
+
     // res.header('Access-Control-Expose-Headers', 'Content-Length');
     res.header('Access-Control-Allow-Credentials', true);
     res.header("preflightContinue", true);
